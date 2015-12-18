e40b7ce44deed20d166092bef9c9d655f82af161
Martin Senne <mail@martin-senne.de>
2015-12-19 00:36:17 +0100

    Splitting test suites.

--- src/test/scala/org/somuchfun/rockpaperscissors/RPSTestSuite.scala
+++ src/test/scala/org/somuchfun/rockpaperscissors/RPSTestSuite.scala
@@ -4,6 +4,28 @@ import org.scalatest.FunSuite
 import org.somuchfun.rockpaperscissors.players.Player
 import org.somuchfun.rockpaperscissors.exceptions._
 
+
+
+class VariantTestSuite extends FunSuite {
+
+  test("The win function works properly.") {
+    val variant = new RegularVariant
+
+    assertResult(Draw)(variant.beats(1, 1))      // Rock equals Rocks
+    assertResult(PlayerIdB)(variant.beats(1, 2)) // Rock fails against Paper
+    assertResult(PlayerIdA)(variant.beats(1, 3)) // Rock beats Scissors
+
+    assertResult(PlayerIdA)(variant.beats(2, 1)) // Paper beats Rocks
+    assertResult(Draw)(variant.beats(2, 2))      // Paper equals Paper
+    assertResult(PlayerIdB)(variant.beats(2, 3)) // Paper fails against Scissors
+
+    assertResult(PlayerIdB)(variant.beats(3, 1)) // Scissors fails against Rocks
+    assertResult(PlayerIdA)(variant.beats(3, 2)) // Scissors beats Paper
+    assertResult(Draw)(variant.beats(3, 3))      // Scissors equals Scissors
+  }
+}
+
+
 class SequencePlayer(val nameAdd: String, val playerDef: PlayerId, sequence: Seq[Int]) extends Player {
 
   val name = "SequencePlayer " + nameAdd
@@ -25,59 +47,42 @@ class DummyPlayer(val playerDef: PlayerId) extends Player {
   override def nextChoice(game: Game): Int = 0
 }
 
-class RPSTestSuite extends FunSuite {
-
-  import Main._
-  
-  test("The win function works properly.") {
-    val variant = new RegularVariant
-
-    assertResult( Draw )( variant.beats( 1, 1 ) )   // Rock equals Rocks
-    assertResult( PlayerIdB )( variant.beats( 1, 2 ) )      // Rock fails against Paper
-    assertResult( PlayerIdA )( variant.beats( 1, 3 ) )      // Rock beats Scissors
-
-    assertResult( PlayerIdA )( variant.beats( 2, 1 ) )      // Paper beats Rocks
-    assertResult( Draw )( variant.beats( 2, 2 ) )   // Paper equals Paper
-    assertResult( PlayerIdB )( variant.beats( 2, 3 ) )      // Paper fails against Scissors
-
-    assertResult( PlayerIdB )( variant.beats( 3, 1 ) )      // Scissors fails against Rocks
-    assertResult( PlayerIdA )( variant.beats( 3, 2 ) )      // Scissors beats Paper
-    assertResult( Draw )( variant.beats( 3, 3 ) )   // Scissors equals Scissors
-  }
-  
+class FullGameTestSuite extends FunSuite {
   // @Todo: Add general test version instead of fixed version.
   test("Game with no draws ends correctly.") {
-    val playerA = new SequencePlayer("A", PlayerIdA, Seq(1,2,3))
-    val playerB = new SequencePlayer("B", PlayerIdB, Seq(3,1,2))
-    
+    val playerA = new SequencePlayer("A", PlayerIdA, Seq(1, 2, 3))
+    val playerB = new SequencePlayer("B", PlayerIdB, Seq(3, 1, 2))
+
     val game = Game(new RegularVariant, playerA, playerB, 3)
     val status = GameSteering.play(game)
-    
-    val comp =  status.completedRounds.toArray
-    
-    assertResult( 3 ) ( comp.size )
-    assertResult( CompletedRound(1, 3, PlayerIdA ) )( comp(0) )
-    assertResult( CompletedRound(2, 1, PlayerIdA ) )( comp(1) )
-    assertResult( CompletedRound(3, 2, PlayerIdA ) )( comp(2) )
+
+    val comp = status.completedRounds.toArray
+
+    assertResult(3)(comp.size)
+    assertResult(CompletedRound(1, 3, PlayerIdA))(comp(0))
+    assertResult(CompletedRound(2, 1, PlayerIdA))(comp(1))
+    assertResult(CompletedRound(3, 2, PlayerIdA))(comp(2))
   }
 
   test("Game with draws ends correctly.") {
-    val playerA = new SequencePlayer("A", PlayerIdA, Seq(1,2,3,2,3))
-    val playerB = new SequencePlayer("B", PlayerIdB, Seq(3,2,3,1,2))
+    val playerA = new SequencePlayer("A", PlayerIdA, Seq(1, 2, 3, 2, 3))
+    val playerB = new SequencePlayer("B", PlayerIdB, Seq(3, 2, 3, 1, 2))
 
     val game = Game(new RegularVariant, playerA, playerB, 3)
     val status = GameSteering.play(game)
 
-    val comp =  status.completedRounds.toArray
+    val comp = status.completedRounds.toArray
 
-    assertResult( 5 ) ( comp.size )
-    assertResult( CompletedRound(1, 3, PlayerIdA ) )( comp(0) )
-    assertResult( CompletedRound(2, 2, Draw ) )( comp(1) )
-    assertResult( CompletedRound(3, 3, Draw ) )( comp(2) )
-    assertResult( CompletedRound(2, 1, PlayerIdA ) )( comp(3) )
-    assertResult( CompletedRound(3, 2, PlayerIdA ) )( comp(4) )
+    assertResult(5)(comp.size)
+    assertResult(CompletedRound(1, 3, PlayerIdA))(comp(0))
+    assertResult(CompletedRound(2, 2, Draw))(comp(1))
+    assertResult(CompletedRound(3, 3, Draw))(comp(2))
+    assertResult(CompletedRound(2, 1, PlayerIdA))(comp(3))
+    assertResult(CompletedRound(3, 2, PlayerIdA))(comp(4))
   }
+}
   
+class SubmitChoicesTestSuite extends FunSuite {
   def preparedGame : Game = {
     val playerA = new DummyPlayer(PlayerIdA)
     val playerB = new DummyPlayer(PlayerIdB)
@@ -90,7 +95,7 @@ class RPSTestSuite extends FunSuite {
     game
   }
   
-  test("Invalid round number is handled correctly by game.") {
+  test("Invalid submitted round number is handled correctly.") {
     val game = preparedGame
 
     val invalidStepResult = game.submitMove(PlayerIdA, 97, 1) // invalid step
